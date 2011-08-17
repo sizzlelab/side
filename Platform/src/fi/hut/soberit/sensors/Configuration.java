1ba66a80735631081b9d7c54e53f945eee4fb150
Maksim <maksim@golivkin.eu>
2011-08-17 12:58:54 +0300

    new copyright notice

--- Platform/src/fi/hut/soberit/sensors/Configuration.java
+++
@@ -1,7 +0,0 @@
-package fi.hut.soberit.sensors;
-
-public class Configuration {
-
-	public static final int SENSORS_BUFFER_SIZE = 10;
-
-}
--- Platform/src/fi/hut/soberit/sensors/generic/ObservationRecord.java
+++
@@ -1,8 +0,0 @@
-package fi.hut.soberit.sensors.generic;
-
-public interface ObservationRecord {
-
-	public long getTime();
-	
-	public byte[] getValue(int i);
-}
