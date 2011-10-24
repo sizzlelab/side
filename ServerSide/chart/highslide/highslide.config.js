b5e29c8667fe92466c7ecb2ff35e419c3f0bfc97
Shuyan Zeng <miyula1988@gmail.com>
2011-10-24 17:21:16 +0300

    debug chart person

--- ServerSide/chart/highslide/highslide.config.js
+++ ServerSide/chart/highslide/highslide.config.js
@@ -1,7 +1,7 @@
 /**
 *	Site-specific configuration settings for Highslide JS
 */
-
+var hs;
 hs.outlineType = 'rounded-white';
 hs.wrapperClassName = 'draggable-header';
 hs.captionEval = 'this.a.title';
