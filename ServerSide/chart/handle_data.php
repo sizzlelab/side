f689769edf6e475a87713e5134866c6c50d6c7bc
yongzhen zhang <4.m@live.cn>
2011-10-26 16:36:10 +0800

    change id

--- ServerSide/chart/handle_data.php
+++ ServerSide/chart/handle_data.php
@@ -21,7 +21,7 @@ echo $result;
 */
 
 $start=$_GET['start'];$perid=$_GET['perid'];$proid=$_GET['proid'];$end=$_GET['end'];
-	$url="http://jimu.cs.hut.fi/side/researcher/observations/data/json?type=2&start=".$start."&end=".$end."&perid=".$perid."&proid=".$proid;
+	$url="http://jimu.cs.hut.fi/side/researcher/observations/data/json?type=0&start=".$start."&end=".$end."&perid=".$perid."&proid=".$proid;
 	$file = file_get_contents($url, true);
 	//$file=str_ireplace('"','',$file);
 	$first_part=substr($file,0,65);
