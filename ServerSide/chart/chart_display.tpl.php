930309fddbc9f17417a30b9e72fd2f20769e769b
yongzhen zhang <4.m@live.cn>
2011-11-01 20:44:52 +0800

    update

--- ServerSide/chart/chart_display.tpl.php
+++ ServerSide/chart/chart_display.tpl.php
@@ -331,7 +331,7 @@ $('#bloodpresure').html(htm);
 <div style="text-align:center;margin-bottom:30px;background-color:#E1E8F0;font-size:20px">
 
 	<select id="project_list" onchange='get_person()' name="project"><option  selected='selected' value="--Choose project--"  >--Choose project--</option></select>
-	<select id="person_list" name="person"><option  selected='selected' value="--Choose person--" >--Choose person--</option></select>
+	<select id="person_list" onchange='draw_blood_preasure_table()' name="person"><option  selected='selected' value="--Choose person--" >--Choose person--</option></select>
 
 </div>
 <div style"=margin:20px;" id="bloodpresure">
