13fece360021b6618a9b73e87d18b3b02532089b
yongzhen zhang <4.m@live.cn>
2011-11-01 17:14:33 +0800

    test

--- ServerSide/chart/handle_data.php
+++ ServerSide/chart/handle_data.php
@@ -27,9 +27,9 @@ $start=$_GET['start'];$perid=$_GET['perid'];$proid=$_GET['proid'];$end=$_GET['en
 	$glucose_result=$glucose_first_part.$glucose_second_part;
 	$position=strpos($glucose_result,'null');
 	if(position){
-		$glucose_string='"records":['.$time_string;
+		//$glucose_string='"records":['.$time_string;
 		
-		$glucose_result=str_ireplace('"records":null',$glucose_string,$glucose_result);
+		//$glucose_result=str_ireplace('"records":null','"records":null]]',$glucose_result);
 		//$glucose_result=str_ireplace('null','[[Date.UTC(2011, 10, 11, 10, 7),64]]',$glucose_result);
 	}
 	echo $result.','.$glucose_result;
