f4c72b9e54b280ab488f40286a451fddcaf980e8
Shuyan Zeng <miyula1988@gmail.com>
2011-11-11 16:43:46 +0200

    fix the structure of chart

--- ServerSide/chart/js/chart_display.js
+++ ServerSide/chart/js/chart_display.js
@@ -1,10 +1,11 @@
 var chart;
 hs.graphicsDir = 'http://highslide.com/highslide/graphics/';
 
-$(document).ready(function() {
+$(function() {
     $("#datepicker").datepicker({showOn: 'button', buttonImage: Drupal.settings.chart.module_path+'/images/calendar.gif', buttonImageOnly: true});
     get_project();
-    $("#project_list").change(function() {
+    $("#person_list").change(function() {
+	//draw_blood_preasure_table();
 	draw_chart();
     });
 });
@@ -22,14 +23,15 @@ function millisecondsStrToDate(str){
         d.setMilliseconds(str); 
         s   =   d.getHours()   +   ":"   +   d.getMinutes()   +   ":"   +   d.getSeconds(); 
         //return d.toLocaleString();
-	return s;
+		return s;
 }
-
 
 function draw_chart(){
     draw_blood_preasure_table();
+    //document.chart_form.submit(); 
     var module_url = Drupal.settings.chart.module_path;
-    var perid=Drupal.settings.chart.current_user;
+    //var data_path = module_url + "/<?php echo 'handle_data.php?type=2&start='.$date_now.'&proid='.$_POST['project'].'&perid='.$_POST['person'];?>";
+    var perid=document.getElementById('person_list').value;
     var proid=document.getElementById('project_list').value;
     var start_str=document.getElementById('datepicker').value;
     var start_arr=new Array();
@@ -37,86 +39,87 @@ function draw_chart(){
     var start=start_arr[2]+'-'+start_arr[0]+'-'+start_arr[1];
     var end=start_arr[2]+'-'+start_arr[0]+'-'+(parseInt(start_arr[1],10)+1);
     var url=Drupal.settings.chart.handle_data+"?start="+start+'&end='+end+'&perid='+perid+'&proid='+proid;
-    $.getJSON(url, function(data1) {
-	//var test=data1.observations[0].records;			
-	var options = {
-	    chart: {
-		renderTo: 'container',
-		defaultSeriesType: 'spline'
-	    },
-	    title: {
-		text: ''
-	    },
-	    credits:{
-		enabled:false
-	    },
-	    xAxis: {
-		//categories: []
-		title:{
-		    text:''
-		},
-		type: 'datetime'
-	    },
-	    tooltip:{
-		shared:true,
-		crosshairs: true
-	    },
-	    yAxis: [{
-		title: {
-		    text: 'Value (bpm)'
-		},
-		max:150,
-		plotLines: [{
-		    value: 0,
-		    width: 1,
-		    color: '#808080'
-		}],
-		min: 0,
-		minorGridLineWidth: 0, 
-		gridLineWidth: 1,
-		alternateGridColor: null,
-		plotBands: [ { //Normal range
-		    from: 60,
-		    to: 100,
-		    color: 'rgba(0, 255, 0, 0.3)',
-		    label: {
-			text: 'Normal',
-			align:'left',
-			x:-45,
-			style: {
-			    color: 'rgba(0, 255, 0, 0.4)'
-			}
-		    }
-		    }, { //High range
-			from: 100,
-			to: 150,
-			color: 'rgba(255, 0, 0, 0.5)',
-			label: {
-			    text: 'Higher',
-			    align:'left',
-			    x:-45,
-			    style: {
-				color: '#FF0000'
-			    }
-			}
-		    },{ //Low range
-			from: 30,
-			to: 60,
-			color: '#FFA500',
-			label: {
-			    text: 'Lower',
-			    align:'left',
-			    x:-35,
-			    style: {
-				color: '#FFA500'
-			    }
-			}
-		    }
-		]
+    $.getJSON(url, function(data1) {	
+					var options = {
 
-		},{
-		title: {
-			text: 'Value ( mg/dl)'
+				chart: {
+					renderTo: 'container',
+					defaultSeriesType: 'spline'
+				},
+				title: {
+					text: ''
+				},
+				credits:{
+						enabled:false
+					},
+				xAxis: {
+					//categories: []
+					title:{
+						text:''
+					},
+					type: 'datetime'
+				},
+					tooltip:{
+						shared:true,
+						crosshairs: true
+					},
+					yAxis: [{
+						title: {
+							text: 'Value (bpm)'
+							
+						},
+						max:150,
+						plotLines: [{
+							value: 0,
+							width: 1,
+							color: '#808080'
+						}],
+						min: 0,
+						minorGridLineWidth: 0, 
+						gridLineWidth: 1,
+						alternateGridColor: null,
+						plotBands: [ { //Normal range
+							from: 60,
+							to: 100,
+							color: 'rgba(0, 255, 0, 0.3)',
+							label: {
+								text: 'Normal',
+								align:'left',
+								x:-45,
+								style: {
+									color: 'rgba(0, 255, 0, 0.4)'
+								}
+							}
+						}, { //High range
+							from: 100,
+							to: 150,
+							color: 'rgba(255, 0, 0, 0.5)',
+							label: {
+								text: 'Higher',
+								align:'left',
+								x:-45,
+								style: {
+									color: '#FF0000'
+								}
+							}
+						},{ //Low range
+							from: 30,
+							to: 60,
+							color: '#FFA500',
+							label: {
+								text: 'Lower',
+								align:'left',
+								x:-35,
+								style: {
+									color: '#FFA500'
+								}
+							}
+						}
+						]
+
+					},{
+					title: {
+							text: 'Value ( mg/dl)'
 							
 						},
 						opposite:true,
@@ -170,7 +173,7 @@ function draw_chart(){
 						]
 					}],
 					plotOptions: {
-					spline: {
+							spline: {
 			showCheckbox:true,
 			selected:true,
 			events: {
@@ -244,30 +247,36 @@ function draw_chart(){
 //});
 }
 
-	
 function get_project(){
-    $.getJSON(Drupal.settings.chart.getpersonprojects,function(results){
-			var outputs='<option selected="selected">--Choose project--</option>';
-		for(x in results){
-			outputs+="<option value='"+results[x]['id']+"'>"+results[x]['name']+"</option>";
-			}
-		$('#project_list').html(outputs);
-		})
+    $.getJSON(Drupal.settings.chart.getprojects,function(results){
+        var outputs='<option selected="selected">--Choose project--</option>';
+	for(x in results){
+	    outputs+="<option value='"+results[x]['nid']+"'>"+results[x]['title']+"</option>";
+	}
+	$('#project_list').html(outputs);
+    })
+}
 	
+function get_person(){
+    var project_id=document.getElementById('project_list').value;
+    //alert(project_id);
+    $.getJSON(Drupal.settings.chart.getprojectpersons+project_id,function(results){
+	var outputs='<option selected="selected">--Choose person--</option>';
+	for(x in results){
+	    outputs+="<option value='"+results[x]['id']+"'>"+results[x]['name']+"</option>";
+	}
+        $('#person_list').empty().html(outputs);
+    })
+    //draw_chart();
 }
 
-function get_blood_presure(proid, start, end ){
-	var perid=Drupal.settings.chart.current_user;//side/researcher/observations/data/json?type=3
-        $.getJSON(Drupal.settings.chart.getpersondata+'?type=3&proid='+proid+'&end='+end+'&start='+start,function(results){
-	
-	    //console.debug(results);
-	    var htm="<table>";        
-	    var obs = results.observations;
-	    //for(x in obs){
-             
-	    htm +="<tr><td><b>"+obs[0]['name']+"</b></td><td></td><td></td><td></td></tr>";
-	    htm += "<tr><td></td><td>Time</td><td>Systolic</td><td>Diastolic</td></tr>";
-	    for(y in obs[0]['records']){
+function get_blood_presure(proid, perid,start, end ){
+    $.getJSON(Drupal.settings.chart.getpersonobservation+'?type=3&proid='+proid+'&perid='+perid+'&end='+end+'&start='+start,function(results){	
+    var htm="<table>";        
+    var obs = results.observations;
+    htm +="<tr><td><b>"+obs[0]['name']+"</b></td><td></td><td></td><td></td></tr>";
+    htm += "<tr><td></td><td>Time</td><td>Systolic</td><td>Diastolic</td></tr>";
+    for(y in obs[0]['records']){
                     htm += "<tr><td>";
                    
                     htm += y;
@@ -278,19 +287,17 @@ function get_blood_presure(proid, start, end ){
                     htm += "</td><td>";   //result.observation3[0].records['systolic'],
                     htm += obs[0]['records'][y]['diastolic'];
                     htm += "</td></tr>";
-	    }
-	    //  }
-          
-	    htm = htm+"</table>";
-	    $('#bloodpresure').html(htm);
-        })
-  }
-  
-  function draw_blood_preasure_table(){
+    }
+    htm = htm+"</table>";
+    $('#bloodpresure').html(htm);
+    })
+}
+
+function draw_blood_preasure_table(){
 		var start_str=document.getElementById('datepicker').value;
 		var start_arr=new Array();
 		start_arr=start_str.split('/');
 		var start=start_arr[2]+'-'+start_arr[0]+'-'+start_arr[1];
 		var end=start_arr[2]+'-'+start_arr[0]+'-'+(parseInt(start_arr[1],10)+1);
-		get_blood_presure($("#project_list option:selected").val(), start, end );
+		get_blood_presure($("#project_list option:selected").val(),$("#person_list option:selected").val(), start, end );
   }
