47e57cb876fb4200417ffd7fea6e7978c6dfb555
Maksim <maksim@golivkin.eu>
2011-11-13 16:00:44 +0200

    The Physical Activity Ant project itself.

---
+++ PhysicalAcitivtyAnt/src/fi/hut/soberit/physicalactivity/PhysicalAcitivtyAntActivity.java
@@ -0,0 +1,14 @@
+package fi.hut.soberit.physicalactivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PhysicalAcitivtyAntActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        throw new RuntimeException("Not implemented");
+    }
+}
