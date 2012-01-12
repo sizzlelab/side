d20f38be784b78e6fe30afa2625f4db6373c3231
Maksim <maksim@golivkin.eu>
2012-01-12 16:53:58 +0200

    forgotten ThermoApp. Not working a the moment, through

---
+++ ThermoApp/src/fi/hut/soberit/sensors/fora/ir21/IR21Application.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Aalto University
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ * Authors:
+ * Maksim Golivkin <maksim@golivkin.eu>
+ ******************************************************************************/
+package fi.hut.soberit.sensors.fora.ir21;
+
+
+import fi.hut.soberit.sensors.fora.ir21.R;
+import android.app.Application;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+public class IR21Application extends Application {
+
+	private static final String TAG = IR21Application.class.getSimpleName();
+
+	@Override 
+	public void onCreate() {
+		Log.d(TAG, "onCreate");
+		
+		PreferenceManager.setDefaultValues(
+				this, 
+				IR21Settings.APP_PREFERENCES_FILE,
+				MODE_PRIVATE,
+				R.xml.preferences, 
+				false);
+	}
+}
