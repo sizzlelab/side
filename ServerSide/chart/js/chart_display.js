0da7f8a431b1b4ee43232d0a2a55c13e532a54e5
yongzhen <4.m@live.cn>
2012-01-28 20:02:38 +0800

    blood pressure chart

--- ServerSide/chart/js/chart_display.js
+++ ServerSide/chart/js/chart_display.js
@@ -282,6 +282,7 @@ function draw_bloodpresure_chart(){
 					var length=temp.length;var x;var i=0;
 					var dia_arr=new Array();
 					var sys_arr=new Array();
+					var dia_str="[";
 					for(i=0;i<length;i++){
 							var obj= temp[i];
 							for(x in obj)
@@ -289,11 +290,13 @@ function draw_bloodpresure_chart(){
 								// document.write(x); diastolic
 								if(x=="diastolic"){
 									dia_arr[obj[x][0]]=obj[x][1];
+									dia_str=dia_str+ obj[x][0]+","+obj[x][1];
 								}else{
 									sys_arr[obj[x][0]]=obj[x][1];
 									}
 										}
-	
+					dia_str=dia_str+"]";
+					var dia_str=JSON.parse(dia_str);
 	/**
 					 arr_systolic=new Array();
 					 arr_diastolic=new Array();
@@ -404,12 +407,9 @@ function draw_bloodpresure_chart(){
 						
 				},
 
-				series: [{
-							data:sys_arr,
-							name:'Systolic'
-						 },
+				series: [
 						 {
-							data:dia_arr,
+							data:dia_str,
 							name:'Diastolic'
 						 }
 						 ]
