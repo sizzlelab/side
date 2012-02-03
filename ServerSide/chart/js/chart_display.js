8f69d6ecc35e0ee7eee72c0c7bd5a9229183da91
yongzhen <4.m@live.cn>
2012-02-03 23:36:59 +0800

    chart x axis

--- ServerSide/chart/js/chart_display.js
+++ ServerSide/chart/js/chart_display.js
@@ -192,11 +192,11 @@ function draw_chart(){
     var start_arr=new Array();
     start_arr=start.split('-');
     start=start_arr[2]+'-'+start_arr[1]+'-'+start_arr[0];
-	var start_utc=eval("Date.UTC("+start_arr[2]+','+parseInt(start_arr[1],10)-1+','+start_arr[0]+')');
+	var start_utc=eval("Date.UTC("+start_arr[2]+','+(parseInt(start_arr[1],10)-1)+','+start_arr[0]+')');
 	var end_arr=new Array();
     end_arr=end.split('-');
     end=end_arr[2]+'-'+end_arr[1]+'-'+end_arr[0];
-	var end_utc=eval("Date.UTC("+end_arr[2]+','+parseInt(end_arr[1],10)-1+','+end_arr[0]+')');
+	var end_utc=eval("Date.UTC("+end_arr[2]+','+(parseInt(end_arr[1],10)-1)+','+end_arr[0]+')');
     var url=Drupal.settings.chart.handle_heart_beat_data+"?start="+start+'&end='+end+'&perid='+perid+'&proid='+proid;
     $.getJSON(url, function(data1) {	
 					var options = {
