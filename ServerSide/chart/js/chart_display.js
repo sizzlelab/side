b56e887250630643fa303a5e979fc5527ed02799
yongzhen <4.m@live.cn>
2012-02-04 20:48:28 +0800

    get person

--- ServerSide/chart/js/chart_display.js
+++ ServerSide/chart/js/chart_display.js
@@ -516,7 +516,7 @@ function get_person(){
 				var project_id=parm;
 			else
 				var project_id=document.getElementById('project_list').value;
-    alert(project_id);
+    //alert(project_id);
     $.getJSON(Drupal.settings.chart.getprojectpersons+project_id,function(results){
 	var outputs='<option selected="selected">--Choose person--</option>';
 	for(x in results){
